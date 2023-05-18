7824ee2518663c681464ca00b3e8f1e1453bd703
Samuel Mundo <samuelmundo@gmail.com>
2023-05-18 13:38:55 -0600

    feature/rea-129-add-modal-component

--- airbnb-clone-app/src/components/hooks/useToggle.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react'
-
-export const useToggle = (initialState: boolean): [boolean, () => void] => {
-  const [state, setState] = useState(() => initialState)
-
-  const toggle = () => setState((prev) => !prev)
-  return [state, toggle]
-}
