d8aef088f127e41ef721aeca01ace3d6e917b376
Samuel Mundo <samuelmundo@gmail.com>
2023-05-17 20:39:01 -0600

    feature/rea-117-add-user-menu-to-the-navbar

---
+++ airbnb-clone-app/src/components/Common/FloatingMenu.styled.ts
@@ -0,0 +1,57 @@
+import styled, { css } from 'styled-components'
+
+type WrapperProps = {
+  width?: string
+  expanded?: boolean
+}
+export const FloatingMenuWrapper = styled.div<WrapperProps>`
+  position: absolute;
+  display: ${({ expanded }) => (expanded ? 'flex' : 'none')};
+  margin: 20px 10px;
+  right: 20px;
+  flex-direction: column;
+  padding: 10px 0;
+  overflow-x: hidden;
+
+  width: ${({ width = 'auto' }) => width};
+  align-items: flex-start;
+  background: #ffffff;
+  box-shadow: 0px 4px 18px rgba(0, 0, 0, 0.17);
+  border-radius: 16px;
+`
+type ItemProps = {
+  fontSize?: string
+  fontWeight?: string
+  separator?: boolean
+}
+
+export const FloatingMenuItem = styled.div<ItemProps>`
+  width: 100%;
+  padding: 8px 16px;
+  font-size: ${({ fontSize = 'font-weight-regular', theme }) =>
+    theme.sizes[fontSize]};
+  font-weight: ${({ fontWeight = 'font-weight-regular', theme }) =>
+    theme.sizes[fontWeight]};
+
+  &:hover {
+    background-color: ${(props) => props.theme.colors['neutral-01']};
+    cursor: pointer;
+  }
+
+  ${({ separator }) =>
+    separator &&
+    css`
+      &:not(:first-child)::before {
+        content: '';
+        display: block;
+        height: 1px;
+        margin: 4px 0;
+        background-color: ${(props) => props.theme.colors['neutral-03']};
+      }
+
+      &:hover {
+        background-color: transparent;
+        cursor: initial;
+      }
+    `}
+`
