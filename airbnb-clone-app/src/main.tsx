18c2b2933fdb712de012c78d98c0ddf51fd59587
Samuel Mundo <samuelmundo@gmail.com>
2023-05-24 10:43:30 -0600

    refactor/router structure

--- airbnb-clone-app/src/main.tsx
+++ airbnb-clone-app/src/main.tsx
@@ -1,23 +1,10 @@
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import { GlobalStyles } from './components/Styles/GlobalStyles'
 import { ThemeProvider } from 'styled-components'
 import { theme } from './components/Styles/theme'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
-
-import Styleguide from './Styleguide'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/styleguide',
-    element: <Styleguide />,
-  },
-])
+import { router } from './router/history'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <AuthProvider>
