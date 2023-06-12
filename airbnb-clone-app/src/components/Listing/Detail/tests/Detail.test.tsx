6a72d90bdb9241b3e79d1e48545b74f9fd164608
Samuel Mundo <samuelmundo@gmail.com>
2023-06-12 08:52:39 -0600

    test/detail-testing

--- airbnb-clone-app/src/components/Listing/Detail/tests/Detail.test.tsx
+++ airbnb-clone-app/src/components/Listing/Detail/tests/Detail.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen, waitFor } from '@testing-library/react'
+import UserEvent from '@testing-library/user-event'
 import { MemoryRouter } from 'react-router-dom'
 import '@testing-library/jest-dom'
 import { Wrapper } from '../../../../../test/jest/__mocks__/wrapperMock'
@@ -7,19 +8,25 @@ import Detail from '../Detail'
 import { generateFakeListing } from '../../../../../test/jest/__mocks__/dataMock'
 
 describe('listing_detail_rendering', () => {
-  it('listing_detail_data_renders_valid', async () => {
-    const mockData = generateFakeListing()
-    jest.spyOn(useListing, 'useListing').mockReturnValue({
-      data: [mockData],
-      loading: false,
-      getPaginatedListing: jest.fn(),
-      getAllListing: jest.fn(),
-      getOneListing: jest.fn(),
-      resetData: jest.fn(),
-      hasMore: false,
-      error: null,
-    })
+  const mockWindowOpen = jest.fn()
 
+  Object.defineProperty(window, 'open', {
+    writable: true,
+    value: mockWindowOpen,
+  })
+  const mockData = generateFakeListing()
+  jest.spyOn(useListing, 'useListing').mockReturnValue({
+    data: [mockData],
+    loading: false,
+    getPaginatedListing: jest.fn(),
+    getAllListing: jest.fn(),
+    getOneListing: jest.fn(),
+    resetData: jest.fn(),
+    hasMore: false,
+    error: null,
+  })
+
+  it('listing_detail_data_renders_valid', async () => {
     render(
       <MemoryRouter>
         <Detail />
@@ -28,7 +35,6 @@ describe('listing_detail_rendering', () => {
     )
 
     await waitFor(() => {
-      //find mockData.address.street as a reges on screen
       expect(
         screen.getByText(new RegExp(mockData.address.street, 'i'))
       ).toBeInTheDocument()
@@ -36,10 +42,24 @@ describe('listing_detail_rendering', () => {
         screen.getByText(new RegExp(mockData.host.host_name, 'i'))
       ).toBeInTheDocument()
 
-      //find all amenities
       mockData.amenities.forEach((amenity) => {
-        expect(screen.getByText(new RegExp(amenity, 'i'))).toBeInTheDocument()
+        expect(screen.getByText(amenity)).toBeInTheDocument()
       })
+    })
+  })
+
+  it('listing_detail_image_click', async () => {
+    render(
+      <MemoryRouter>
+        <Detail />
+      </MemoryRouter>,
+      { wrapper: Wrapper }
+    )
+    const image = screen.getAllByAltText('listing')[0]
+    await UserEvent.click(image)
+
+    await waitFor(() => {
+      expect(mockWindowOpen).toBeCalledTimes(1)
     })
   })
 })
